f32fee6b74c712cc1f195c4ca7cd44a5df20b55f
acostajs <acosta.juan@icloud.com>
2025-04-08 18:48:36 -0400

    update

--- Prototype_01/app/__init__.py
+++ Prototype_01/app/__init__.py
@@ -3,9 +3,11 @@
 from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail 
 import os
 
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app(config_class=Config):
     app = Flask(__name__, instance_relative_config=True)
@@ -19,6 +21,7 @@ def create_app(config_class=Config):
 
     # Initialize extensions
     db.init_app(app)
+    mail.init_app(app)
 
     # --- Register Blueprints ---
     # Import the blueprint instance from your routes module
