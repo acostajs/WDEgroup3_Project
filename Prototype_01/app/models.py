3a07942f49979f0490759801fe13f1ce565fb8d5
acostajs <acosta.juan@icloud.com>
2025-04-09 13:39:34 -0400

    Update models.py

--- Prototype_01/app/models.py
+++ Prototype_01/app/models.py
@@ -13,14 +13,16 @@ class Employee(db.Model):
 
 class Shift(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'))
-    start_time = db.Column(db.DateTime, index=True)
-    end_time = db.Column(db.DateTime)
-
+    employee_id = db.Column(db.Integer, db.ForeignKey('employee.id'), nullable=True, index=True)
+    start_time = db.Column(db.DateTime, nullable=False, index=True)
+    end_time = db.Column(db.DateTime, nullable=False)
+    required_position = db.Column(db.String(64), nullable=False, index=True) 
     employee = db.relationship('Employee', backref='shifts')
 
     def __repr__(self):
-        return f'<Shift {self.employee.name} {self.start_time}>'
+        emp_name = self.employee.name if self.employee else "Unassigned"
+        return f'<Shift P:{self.required_position} E:{emp_name} Start:{self.start_time.strftime("%H:%M")}>'
+
     
 class PerformanceLog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,7 +31,6 @@ class PerformanceLog(db.Model):
     rating = db.Column(db.Float) 
     notes = db.Column(db.Text) 
     recorded_at = db.Column(db.DateTime, default=datetime.datetime.utcnow) 
-
     employee = db.relationship('Employee', backref=db.backref('performance_logs', lazy='dynamic'))
 
     def __repr__(self):
